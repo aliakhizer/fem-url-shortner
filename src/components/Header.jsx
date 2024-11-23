180cb48cc273f0f6309009a4da572e86ad206f00
Alia Khizer <aliakhizar6@gmail.com>
2024-11-23 23:55:12 +0500

    feat: add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,30 @@
+import Logo from "./icons/Logo";
+import Menu from "./icons/Menu";
+
 export default function Header() {
-  return <header>Header</header>
+  return (
+    <header className="container py-8 mx-auto w-[90%] justify-between flex items-center">
+      <div className="flex items-center gap-8">
+        <Logo />
+        <ul className="md:flex hidden items-center gap-4">
+          <li>
+            <a href="">features</a>
+          </li>
+          <li>
+            <a href="">pricing</a>
+          </li>
+          <li>
+            <a href="">resources</a>
+          </li>
+        </ul>
+      </div>
+      <div className="md:flex hidden items-center gap-12">
+        <div>a</div>
+        <div>b</div>
+      </div>
+      <div className="md:hidden">
+        <Menu />
+      </div>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          violet: "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          grayishViolet: "hsl(257, 7%, 63%)",
+          veryDarkBlue: "hsl(255, 11%, 22%)",
+          veryDarkViolet: "hsl(260, 8%, 14%)",
+        },
+      },
+      fontSize: {
+        body: "18px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
 };
