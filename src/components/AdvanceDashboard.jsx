1dc02022de28ed51636ac8a62ae8ca95d10a1df3
Alia Khizer <aliakhizar6@gmail.com>
2024-11-27 16:06:14 +0500

    adding card styling

--- src/components/AdvanceDashboard.jsx
+++ src/components/AdvanceDashboard.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Card from "./Card";
+import BrandLogo from "./icons/BrandLogo";
+import IconsDetails from "./icons/IconsDetails";
+import FullyCustomized from "./icons/FullyCustomized";
 
 export default function AdvanceDashboard() {
-  return <>
-  
-  </>
+  return (
+    <>
+      <div className="bg-gray-300 py-28 ">
+        <div className="container mx-auto flex flex-col justify-center items-center gap-6">
+          <h1 className="font-bold capitalize text-primary-violet text-3xl">
+            Advance Statistics
+          </h1>
+          <p className="text-neutral-grayishViolet capitalized md:max-w-96 text-center">
+            Track how your links are performing across the web with our advanced
+            statistics.
+          </p>
+        </div>
+
+        <div className="container mx-auto flex flex-col md:flex-row py-16">
+          <div className="flex justify-center">
+            <Card icon={BrandLogo} title="Brand Recognition" />
+          </div>
+          <div className="flex justify-center items-center ">
+            <div className=" border-l-8 h-20 border-primary-cyan  md:border-t-8  md:h-0 md:w-6 md:mt-6 "></div>
+          </div>
+
+          <div className="flex justify-center md:mt-14">
+            <Card icon={IconsDetails} title="Icons Details" />
+          </div>
+          <div className="flex justify-center items-center">
+            <div className=" border-l-8 h-20 border-primary-cyan  md:border-t-8  md:h-0 md:w-6 md:mt-6 "></div>
+          </div>
+
+          <div className="flex justify-center  md:mt-20">
+            <Card icon={FullyCustomized} title="Fully Recognition" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import BrandLogo from "./icons/BrandLogo";
+
+export default function Card({ icon: Icon, title: Title }) {
+  return (
+    <>
+      <div className=" justify-center second:mt-10   ">
+        <div className=" container mx-auto relative max-w-sm w-full bg-white shadow-md p-8 rounded-lg">
+          <div className="absolute  -top-10 left-1/2 transform -translate-x-1/2  md:left-4 md:top-[-32px] md:transform-none bg-primary-violet p-4 rounded-full">
+            {<Icon />}
+          </div>
+
+          <div className="mt-10">
+            <h1 className="text-xl font-semibold text-gray-800"> {Title}</h1>
+            <p className=" container mx-auto mt-2 text-gray-400 capitalize">
+              Boost your recognition with each click. Generic links do not mean
+              a thing. Branded links help instill confidence in your content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
